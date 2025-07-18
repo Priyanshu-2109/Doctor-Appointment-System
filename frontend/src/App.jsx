074c0ae6a6ef1579a56b8b5d44db1df4a76d4431
Zeel190805 <jeelbarvaliya1985@gmail.com>
2025-07-18 23:44:10 +0530

    Add Banner, Footer, TopDoctors, and AppContext
    Introduced Banner, Footer, and TopDoctors components to enhance the Home page and overall layout. Added AppContext and provider for managing doctor data. Updated App.jsx to include Footer, and wrapped the app in AppContextProvider in main.jsx. Minor HTML and CSS adjustments for improved UI.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import Doctors from './pages/Doctors'
-import Contect from './pages/Contect'
-import Login from './pages/Login'
-import MyProfile from './pages/MyProfile'
-import MyAppointments from './pages/MyAppointments'
-import Appointment from './pages/Appointment'
-import Navbar from './components/Navbar'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Doctors from "./pages/Doctors";
+import Contect from "./pages/Contect";
+import Login from "./pages/Login";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <div className='mx-4 sm:mx-[10%]'>
+    <div className="mx-4 sm:mx-[10%]">
       <Navbar />
-      
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/doctors' element={<Doctors />} />
-        <Route path='/doctors/:spaciality' element={<Doctors />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/contact' element={<Contect />} />
-        <Route path='/my-profile' element={<MyProfile />} />
-        <Route path='/my-appointments' element={<MyAppointments />} />
-        <Route path='/appointment/:docId' element={<Appointment />} />
-      </Routes>
-    </div>
-  )
-}
 
-export default App
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/doctors/:spaciality" element={<Doctors />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contect />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/appointment/:docId" element={<Appointment />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
