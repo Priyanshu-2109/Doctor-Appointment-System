fc0913a08be30abf69f579f3521b475d0dd70178
Priyanshu-2109 <142998167+Priyanshu-2109@users.noreply.github.com>
2025-07-30 19:49:14 +0530

    Add admin doctor API and user model setup
    Introduced an admin API endpoint to add doctors, including file upload support via multer middleware. Added user model schema, admin and doctor route files, and integrated the admin route into the server. Initial controller logic for adding a doctor is included.

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,18 @@
+
+
+
+// add doctor api from admin pannel
+const addDoctor = async (req, res) => {
+    try {
+
+        const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
+
+        console.log({name, email, password, speciality, degree, experience, about, fees, address, imageFile});
+
+    } catch (error) {
+        
+    }
+}
+
+export { addDoctor };
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 //app config
 const app = express()
@@ -17,8 +18,11 @@ app.use(cors())
 
 
 //api endpoints
+app.use('/api/admin', adminRouter);
+// localhost:4000/api/admin
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('API Working!')
 })
 
 app.listen(port, () => {
